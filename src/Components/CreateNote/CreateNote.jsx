878f98622ed0e77ae7ce4f8ea90eb3219da48f77
Sukanta Das <arnobh84@gmail.com>
2023-02-22 20:36:24 +0600

    work completed

--- src/Components/CreateNote/CreateNote.jsx
+++ src/Components/CreateNote/CreateNote.jsx
@@ -5,9 +5,10 @@ import Zoom from "@mui/material/Zoom";
 import "./note.css";
 import { AuthContext } from "../../Context/AuthProvider";
 import { getUser } from "../../helper/getUser";
-
-const CreateNote = (props) => {
-  const [isExpanded, setExpanded] = useState(false);
+import toast, { Toaster } from 'react-hot-toast';
+import { Audio } from 'react-loader-spinner'
+const CreateNote = () => {
+  const [loading, setLoading] = useState(false)
   const [userInfo, setUserInfo] = useState("");
   const { user } = useContext(AuthContext)
 
@@ -26,11 +27,33 @@ const CreateNote = (props) => {
     const form = e.target;
     const title = form.title.value;
     const content = form.content.value;
-    setExpanded(false);
+    const userEmail = userInfo?.email;
+    const userProfile = userInfo?.profilePicture
+    const note = {
+      title, content, userEmail, userProfile
+    }
+    fetch("http://localhost:8000/api/v1/note", {
+      method: "POST",
+      headers: {
+        "content-type": "application/json"
+      },
+      body: JSON.stringify(note)
+    })
+      .then(res => res.json())
+      .then(message => {
+        if (message.status === 200) {
+          toast.success('Notes Saved');
+        }
+
+      })
+
+    form.reset()
+
   };
 
   return (
     <div className="w-[90%] lg:w-3/4 mx-auto">
+      <Toaster></Toaster>
       <form className="create-note relative" onSubmit={handleSubmit}>
         <input name="title" placeholder="Title" />
         <textarea
@@ -40,7 +63,7 @@ const CreateNote = (props) => {
           rows={5}
         />
 
-        <button className=" bg-[#f5ba13] text-white px-3 py-2 rounded">Add Note</button>
+        <button className="bg-[#f5ba13] text-white px-3 py-2 rounded" type="submit">Add Note</button>
       </form>
     </div>
   );
