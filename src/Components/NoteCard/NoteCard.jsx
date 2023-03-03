9d19e53e6e10118a9991b6a0b4b359b87c6cdd1b
Sukanta Das <arnobh84@gmail.com>
2023-03-03 10:57:13 +0600

    previus card component removed

--- src/Components/NoteCard/NoteCard.jsx
+++ src/Components/NoteCard/NoteCard.jsx
@@ -8,37 +8,16 @@ import getRelativeDateString from "../../helper/getRelativeTime";
 // // }, 1000)
 
 const NoteCard = ({ note }) => {
-    console.log(note);
     const { title, content, userProfile, date } = note;
     return (
-        <div>
-            <div class="px-4 sm:px-6 lg:px-5">
-                <div class="w-full px-8 py-8 rounded-md shadow-lg bg-white">
-                    <div class=" flex items-center space-x-2">
-                        <div class="flex flex-shrink-0 rounded-full border border-gray-200">
-                            <img
-                                class="w-8 h-8 object-cover rounded-full"
-                                src={userProfile}
-                                alt=""
-                            />
-                        </div>
-                    </div>
-                    <div class="space-y-1 mt-3 h-24">
-                        <h3 class="font-semibold text-gray-800">{title}</h3>
-                        <p class="text-sm font-medium leading-5text-gray-600 h-24">
-                            {content}
-                        </p>
-                    </div>
+        <div className="shadow-md mx-10 bg-white rounded">
+            <div className="w-full">
+                {/* profile div  */}
+                <div className="profile_Image">
+                    <img src={userProfile} className="h-10 w-10" alt="" />
 
-                    <div class="mt-2 flex items-center space-x-2 justify-between ">
-                        <button className="bg-[#fcc631] text-white px-3 py-1 rounded transition-all duration-500 hover:scale-110">
-                            View
-                        </button>
-                        <span class="text-sm font-semibold leading-5 text-gray-900">
-                            {getRelativeDateString(date)}
-                        </span>
-                    </div>
                 </div>
+
             </div>
         </div>
     );
